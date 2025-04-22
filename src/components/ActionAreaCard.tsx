254ae0fa5f57cb18f5f8d704b9a61ebbdbd117b0
Darya Semenova <ya.piyavkaa@gmail.com>
2025-04-22 11:45:05 +0300

    new functionality added to the main page

--- src/components/ActionAreaCard.tsx
+++ src/components/ActionAreaCard.tsx
@@ -4,85 +4,85 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// @ts-ignore
+import 'swiper/css';
+// @ts-ignore
+import 'swiper/css/scrollbar';
+
+import { Scrollbar } from 'swiper/modules';
+
 export default function ActionAreaCard() {
     return (
-        <div className="cards-container">
-            <Card sx={{ maxWidth: 300 }}>
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="240"
-                        image="/src/assets/card1.png"
-                        alt="bicycle"
-                    />
-                    <CardContent sx={{ alignItems: 'center', display: 'flex', flexDirection: 'column'}}>
-                        <Typography gutterBottom variant="h3" sx={{ fontWeight: 600, fontSize: 20, fontFamily: 'Montserrat'}}>
-                            Прокат велосипедов
-                        </Typography>
-                        <Typography variant="body2" sx={{ color: 'text.secondary', fontWeight: 400, fontSize: 14, fontFamily: 'Montserrat', textAlign:'justify'}}>
-                            аренда велосипедов для прогулок и активного отдыха на свежем воздухе
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-
-            <Card sx={{ maxWidth: 300 }}>
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="240"
-                        image="/src/assets/card3.png"
-                        alt="quad_bike"
-                    />
-                    <CardContent sx={{ alignItems: 'center', display: 'flex', flexDirection: 'column'}}>
-                        <Typography gutterBottom variant="h3" sx={{ fontWeight: 600, fontSize: 20, fontFamily: 'Montserrat'}}>
-                            Квадроциклы
-                        </Typography>
-                        <Typography variant="body2" sx={{ color: 'text.secondary', fontWeight: 400, fontSize: 14, fontFamily: 'Montserrat', textAlign:'justify'}}>
-                            захватывающие поездки по лесным тропам и бездорожью на мощных квадроциклах
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-
-            <Card sx={{ maxWidth: 300 }}>
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="240"
-                        image="/src/assets/card4.png"
-                        alt="sup"
-                    />
-                    <CardContent sx={{ alignItems: 'center', display: 'flex', flexDirection: 'column'}}>
-                        <Typography gutterBottom variant="h3" sx={{ fontWeight: 600, fontSize: 20, fontFamily: 'Montserrat'}}>
-                            Катание на сапах
-                        </Typography>
-                        <Typography variant="body2" sx={{ color: 'text.secondary', fontWeight: 400, fontSize: 14, fontFamily: 'Montserrat', textAlign:'justify'}}>
-                            спокойное и увлекательное плавание на сапборде по живописным водоёмам
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-
-            <Card sx={{ maxWidth: 300 }}>
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="240"
-                        image="/src/assets/card5.png"
-                        alt="stars"
-                    />
-                    <CardContent sx={{ alignItems: 'center', display: 'flex', flexDirection: 'column'}}>
-                        <Typography gutterBottom variant="h3" sx={{ fontWeight: 600, fontSize: 20, fontFamily: 'Montserrat'}}>
-                            Звёздные вечера
-                        </Typography>
-                        <Typography variant="body2" sx={{ color: 'text.secondary', fontWeight: 400, fontSize: 14, fontFamily: 'Montserrat', textAlign:'justify'}}>
-                            вечерние наблюдения за звёздным небом с телескопом и рассказами о космосе
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-
+        <div className="cards-slider">
+            <Swiper className="custom-swiper"
+                modules={[Scrollbar]}
+                spaceBetween={16}
+                scrollbar={{ draggable: true }}
+                touchMoveStopPropagation={false}
+                breakpoints={{
+                    0: { slidesPerView: 1.2,},
+                    600: { slidesPerView: 2.4,},
+                    900: {slidesPerView: 3.4,},
+                }}
+            >
+                {[{
+                    title: "Прокат велосипедов",
+                    image: "/src/assets/card1.png",
+                    desc: "аренда велосипедов для прогулок и активного отдыха на свежем воздухе"
+                }, {
+                    title: "Квадроциклы",
+                    image: "/src/assets/card3.png",
+                    desc: "захватывающие поездки по лесным тропам и бездорожью на мощных квадроциклах"
+                }, {
+                    title: "Баня с чаном",
+                    image: "/src/assets/card2.png",
+                    desc: "расслабляющий отдых в жаркой бане и горячем чанe под открытым небом"
+                }, {
+                    title: "Катание на сапах",
+                    image: "/src/assets/card4.png",
+                    desc: "спокойное и увлекательное плавание на сапборде по живописным водоёмам"
+                }, {
+                    title: "Звёздные вечера",
+                    image: "/src/assets/card5.png",
+                    desc: "вечерние наблюдения за звёздным небом с телескопом и рассказами о космосе"
+                }].map((card, index) => (
+                    <SwiperSlide key={index}>
+                        <Card
+                            sx={{ maxWidth: 300, height: 370, margin: 1}}>
+                            <CardActionArea>
+                                <CardMedia
+                                    component="img"
+                                    height="240"
+                                    image={card.image}
+                                    alt={card.title}
+                                />
+                                <CardContent sx={{ alignItems: 'center', display: 'flex', flexDirection: 'column'}}>
+                                    <Typography gutterBottom variant="h3" sx={{ fontWeight: 600, fontSize: 20, fontFamily: 'Montserrat'}}>
+                                        {card.title}
+                                    </Typography>
+                                    <Typography
+                                        variant="body2"
+                                        sx={{
+                                            color: 'text.secondary',
+                                            fontWeight: 400,
+                                            fontSize: 14,
+                                            fontFamily: 'Montserrat',
+                                            textAlign:'justify',
+                                            overflow: 'hidden',
+                                            textOverflow: 'ellipsis',
+                                            display: '-webkit-box',
+                                            WebkitLineClamp: 3,
+                                            WebkitBoxOrient: 'vertical'}}>
+                                        {card.desc}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </div>
     );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,24 @@
-import {Component} from 'react';
+import {useState} from 'react';
 
 import { SiVk } from "react-icons/si";
 import { FaTelegram } from "react-icons/fa";
 import { MdOutlinePhoneIphone } from "react-icons/md";
 import { FaLocationDot } from "react-icons/fa6";
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
-class Header extends Component {
-    render() {
+import logo from '/src/assets/logo.png';
+
+export const Header = () => {
+        const [isOpen, setIsOpen] = useState(false);
         return (
             <header>
                 <div className="header-container">
-                    <span className="logo">Quiet Grove</span>
+                    <div className="logo-container">
+                        <img src={logo} alt="Logo" />
+                        <span className="logo">Тихая Роща</span>
+                    </div>
+
 
                     <div className="header-icons">
                         <SiVk className="header-icon"/>
@@ -26,19 +34,37 @@ class Header extends Component {
                             <span className="header-text">Нижегородская область,<br />посёлок Ильиногорск</span>
                         </div>
                     </div>
-
-
                 </div>
-                <ul className='nav'>
-                    <li>Дома</li>
-                    <li>Развлечения</li>
-                    <li>Мероприятия</li>
-                    <li>Банный комплекс</li>
-                    <li>Вход</li>
-                </ul>
+                <nav className={`header-nav ${isOpen ? 'active' : ''}`}>
+                    <ul>
+                        <li><a href='#'>Дома</a></li>
+                        <li><a href='#'>Развлечения</a></li>
+                        <li><a href='#'>Мероприятия</a></li>
+                        <li><a href='#'>Банный комплекс</a></li>
+                        <li><a href='#'>Вход</a></li>
+                    </ul>
+
+                    <div className="mobile-contacts">
+                        <div className="vk">
+                            <SiVk className="header-icon" />
+                            <span className="header-text">@quiet_grove</span>
+                        </div>
+                        <div className="tg">
+                            <FaTelegram className="header-icon" />
+                            <span className="header-text">@quiet_grove</span>
+                        </div>
+                        <div className="phone">
+                            <MdOutlinePhoneIphone className="header-icon" />
+                            <span className="header-text">+7 (986) 742 70 80</span>
+                        </div>
+                        <div className="location">
+                            <FaLocationDot className="header-icon" />
+                            <span className="header-text">посёлок Ильиногорск</span>
+                        </div>
+                    </div>
+                </nav>
+                <button className="menu-button-icon" onClick={()=> setIsOpen(!isOpen)}>
+                    {isOpen ? <CloseIcon/> : <MenuIcon/>}</button>
             </header>
         );
-    }
-}
-
-export default Header;
+};
